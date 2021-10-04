e5795fbddf2d6c135ce0c3c9fe70fb0a50873283
naitmerzoug <nayar22@hotmail.fr>
2021-10-04 19:48:21 +0200

    Constructeur de la machine, Implémentation de la méthode read()

--- src/MiniProjet3/TurringMachine.java
+++ src/MiniProjet3/TurringMachine.java
@@ -2,5 +2,48 @@ package MiniProjet3;
 
 public class TurringMachine {
 
+    private int state;
+    private String band;
+    private Head head;
+    private Transition transition [][];
+
+    /**
+     * L'appel au constructeur de la machine de turing permetterai l'initialisation de cette dérniere avec une nouvelle tete de lecture
+     * située à la position initiale souhaitée, une bande de lecture vide, ainsi qu'une table de transition bidimitionnel innitialement vide.
+     *
+     *
+     */
+    public TurringMachine(Transition[][] transition){
+        this.state = Machine.Initial_State;
+        this.band = "";
+        head.setPosition(Machine.Initial_Position);
+        this.transition = transition;
+
+    }
+
+    private char read(){
+        while (head.getPosition()<0){
+            band += Machine.eps;
+            head.avancer();
+        }
+        while (head.getPosition()>=band.length()){
+            band += Machine.eps;
+
+        }
+        return band.charAt(head.getPosition());
+    }
+
+
+
+    private void write(){
+
+    }
+
+    public String toString(){
+
+        return null;
+    }
+
+
 
 }
