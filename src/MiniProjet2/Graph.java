30a7cc62365d787f70f49a12a8bf559ea2d2a7e0
BlackFox3000 <flo-ti@hotmail.fr>
2021-11-03 10:08:03 +0100

    Mini projet 2 Question 4 : incorrecte à finir

--- src/MiniProjet2/Graph.java
+++ src/MiniProjet2/Graph.java
@@ -3,31 +3,14 @@ package MiniProjet2;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Graph {
+public class Graph implements Cloneable {
     public int [][] graph;
 
     public Graph(int[][] graph) {
         this.graph = graph;
     }
 
-    /**
-     * Question 1
-     * Vérifie si le sous ensemble donné est une zone dense du graphe
-     * complexité : n^2
-     * @param sousEnsemble
-     * @return boolean;
-     */
-    public boolean estDense(ArrayList<Integer> sousEnsemble ) {
-        for(Integer i : sousEnsemble){
-            for (Integer j : sousEnsemble) {
-                if (graph[i][j] != 1)
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
         // x A B C D E F G
         // A 1 1 1 1 1 0 0
         // B 1 1 1 1 0 1 1
@@ -48,6 +31,8 @@ public class Graph {
                 };
         Graph graph = new Graph(matrix);
         int letter = 0;
+
+        System.out.println("====== Question  1 ======");
         ArrayList<Integer> pointToDenseArea = graph.getLinks(letter,new HashMap<>(), false);
         graph.printGraph();
 
@@ -55,10 +40,55 @@ public class Graph {
         for (int i=0; i<pointToDenseArea.size(); i++) {
             System.out.println(graph.getCharForNumber(pointToDenseArea.get(i)));
         }
-        System.out.println("============");
-        ArrayList<Integer> results = graph.foundDenseGroup(letter);
-        for (int result : results)
-            System.out.println(graph.getCharForNumber(result));
+        System.out.println("====== Question  2 ======");
+        ArrayList<Integer> results2 = graph.foundDenseGroup(letter);
+        String resultQuestion2 = "result: ";
+        for (int result : results2)
+            resultQuestion2 += graph.getCharForNumber(result);
+        System.out.println( resultQuestion2);
+
+        System.out.println("====== Question  3 ======");
+        ArrayList<Integer> results3 = graph.foundMaxDenseGroup();
+        String resultQuestion3 = "result: ";
+        for (int result : results3)
+            resultQuestion3 += graph.getCharForNumber(result);
+        System.out.println( resultQuestion3);
+
+        System.out.println("====== Question  4 ====== Non terminé. Retour incorrecte");
+        int [][] matrix2 =
+                {
+                        {1,1,1,1,1,0,0},
+                        {1,1,1,1,0,1,1},
+                        {1,1,1,1,0,0,0},
+                        {1,1,1,1,1,0,0},
+                        {1,0,0,1,1,0,0},
+                        {0,1,0,0,0,1,0},
+                        {0,1,0,0,0,1,0}
+                };
+        Graph graph2 = new Graph(matrix2);
+        System.out.println("test:"+graph.graph[0][5]);
+        graph2.foundSpeedMaxDenseGroup().printGraph();
+
+    }
+    /**
+     *  Question 1 =========================================================
+     */
+
+    /**
+     * Question 1
+     * Vérifie si le sous ensemble donné est une zone dense du graphe
+     * complexité : n^2
+     * @param sousEnsemble
+     * @return boolean;
+     */
+    public boolean estDense(ArrayList<Integer> sousEnsemble ) {
+        for(Integer i : sousEnsemble){
+            for (Integer j : sousEnsemble) {
+                if (graph[i][j] != 1)
+                    return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -77,6 +107,11 @@ public class Graph {
                             hashMap.put(i, new ArrayList<>()); // stock < voisin , point >
                             hashMap.get(i).add(i);
                         }
+                        if (!hashMap.containsKey(point)) {
+                            hashMap.put(point, new ArrayList<>()); // stock < voisin , point >
+                            hashMap.get(point).add(point);
+                        }
+                        System.out.println("point"+getCharForNumber(point));
                         if(! hashMap.get(point).contains(i))
                             hashMap.get(point).add(i);
                     }
@@ -86,12 +121,16 @@ public class Graph {
     }
 
     /**
-     *Question 2
+     *  Question 2 =========================================================
+     */
+
+    /**
+     * Question 2
+     * Complexité O(n^6)
      */
     public ArrayList<Integer> foundDenseGroup(int point) { // O(n^6)
         HashMap<Integer, ArrayList<Integer>> stock = new HashMap<>();
         ArrayList<Integer> result = new ArrayList<>();
-        result.add(point);
 
         // On cherche les voisins du point initiale
         ArrayList<Integer> voisins = getLinks(point, stock, true);
@@ -119,20 +158,6 @@ public class Graph {
         //===================
     }
 
-    /**
-     * Debug
-     * @param stock
-     */
-    private void printStock(HashMap<Integer, ArrayList<Integer>> stock) {
-        for(int key : stock.keySet()){
-            StringBuilder string = new StringBuilder(getCharForNumber(key) + " | ");
-            for (int value : stock.get(key)){
-                string.append(getCharForNumber(value)).append(" ");
-            }
-            System.out.println(string);
-        }
-    }
-
     private ArrayList<Integer> getMinus(HashMap<Integer, ArrayList<Integer>> stock) { // O(n)
         System.out.println(" •• Search Minus ••");
 
@@ -158,6 +183,12 @@ public class Graph {
         return minus;
     }
 
+    /**
+     * complexité O(n^2)
+     * @param stock
+     * @param minus
+     * @return
+     */
     private HashMap<Integer, ArrayList<Integer>> removeMinus(HashMap<Integer, ArrayList<Integer>> stock, ArrayList<Integer> minus) {
         System.out.println(" ** Remove Minus **");
         //Pour chaque valeur min O(n^2)
@@ -165,17 +196,17 @@ public class Graph {
             System.out.println("min:"+getCharForNumber(min));
             for(int key : stock.keySet()) { // O(n)
                 System.out.println("- key:" + getCharForNumber(key));
-                System.out.println("contain : "+stock.get(key).contains(min)+ " && "+ (min != key));
+                //System.out.println("contain : "+stock.get(key).contains(min)+ " && "+ (min != key));
                 if (stock.get(key).contains(min) && min != key) {
                     System.out.println("Remove>"+getCharForNumber(min));
                     stock.get(key).remove( stock.get(key).lastIndexOf(min));
                 }
-                printStock(stock);
             }
             if(stock.keySet().contains(min)) {
                 stock.remove(min);
                 System.out.println("Remove>"+getCharForNumber(min));
             }
+            printStock(stock);
         }
         return stock;
     }
@@ -198,7 +229,7 @@ public class Graph {
                 }
                 for (int num1 : stock.get(key)) {
 
-                    System.out.println("test si: " + getCharForNumber(key2) + " contient " + getCharForNumber(num1));
+                    //System.out.println("test si: " + getCharForNumber(key2) + " contient " + getCharForNumber(num1));
 
                     //On cherche ici si les de sous ensembles comportent les même points qu'importe son ordre
                     if (!stock.get(key2).contains(num1)) {
@@ -213,6 +244,111 @@ public class Graph {
         return true;
     }
 
+    /**
+     *  Question 3 =========================================================
+     */
+
+    /**
+     * Complexité O(n^7)
+     * @return
+     */
+    private ArrayList<Integer> foundMaxDenseGroup() {
+        ArrayList<Integer> denseZoneMax = new ArrayList<>();
+        for(int i = 0 ; i<graph.length ; i++){
+            ArrayList<Integer> denseZone =foundDenseGroup(i);// O(n^6)
+            if(denseZoneMax.size()<denseZone.size())
+                denseZoneMax=denseZone;
+        }
+        return denseZoneMax;
+    }
+
+    /**
+     *  Question 4  =========================================================
+     */
+
+    /**
+     * Non terminé. Retour incorrecte
+     * @return
+     */
+    private Graph foundSpeedMaxDenseGroup() throws CloneNotSupportedException {
+        Graph graph1 = (Graph) this.clone();
+        graph1.printGraph();
+        System.out.println("=============");
+        // faire tant que branches restants ne sont pas égales
+        while (! graph1.branchEquals()) { // O(2n^2)
+            //trouver les branches min
+            int minBranch = getMinBranch(); //O(2n^2)
+            //retirer les branches min
+            graph1.removeBranch(minBranch);
+            graph1.printGraph();
+            System.out.println("=============");
+        }
+
+        return graph1;
+    }
+
+    private void removeBranch(int minBranch) {
+        for(int i=0; i<graph.length ; i++){ // O(2n^2)
+            System.out.println("remove :"+getCharForNumber(i));
+            ArrayList<Integer> voisins = getLinks(i, new HashMap<>(), false);
+
+            if(voisins.size()==minBranch) {
+                for (int voisin : voisins) {
+                    System.out.println("remove :"+getCharForNumber(i)+" of "+getCharForNumber(voisin));
+                    graph[i][voisin] = 0;
+                    graph[voisin][i] = 0;
+                }
+            }
+        }
+    }
+
+    private int getMinBranch(){ //O(2n^2)
+        int nbVoisin = 0;
+        for(int i=0; i<graph.length ; i++){ // O(2n^2)
+            int voisins = getLinks(i, new HashMap<>(), false).size();
+            if(voisins!=0) {
+                if (nbVoisin == 0)
+                    nbVoisin = voisins;
+                if (nbVoisin > voisins)
+                    nbVoisin = voisins;
+            }
+        }
+        System.out.println("Minbranche :"+nbVoisin);
+        return nbVoisin;
+    }
+
+    private boolean branchEquals() {
+        int nbVoisin = 0;
+        for(int i=0; i<graph.length ; i++){ // O(2n^2)
+            int voisins = getLinks(i, new HashMap<>(), false).size();
+            if(voisins!=0) {
+                if (nbVoisin == 0)
+                    nbVoisin = voisins;
+                if (nbVoisin != voisins)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * =========================================================
+     */
+
+    /**
+     * Debug
+     * @param stock
+     */
+    private void printStock(HashMap<Integer, ArrayList<Integer>> stock) {
+        for(int key : stock.keySet()){
+            StringBuilder string = new StringBuilder(getCharForNumber(key) + " | ");
+            for (int value : stock.get(key)){
+                string.append(getCharForNumber(value)).append(" ");
+            }
+            System.out.println(string);
+        }
+    }
 
     public void printGraph(){
         StringBuilder line = new StringBuilder();
@@ -233,5 +369,7 @@ public class Graph {
         }
         return Character.toString(alphabet[i]);
     }
+
+
 
 }
